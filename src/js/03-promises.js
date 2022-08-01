5f607ea7bc0be1a0c4f9f4d56b2f0b3743a69acc
Peter <peteryurich@gmail.com>
2022-08-02 00:47:44 +0300

    ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import notiflix from 'notiflix'
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const shouldResolve = Math.random() > 0.5;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
+  promise
+    .then(( {position, delay} ) => {
+      notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`, 8000);
+    })
+    .catch(( {position, delay} ) => {
+      notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`, 8000);
+    });
 }
+
+formEl = document.querySelector('.form')
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const firstDelay = document.querySelector('[name="delay"]').value
+  const stepTime = document.querySelector('[name="step"]').value
+  const amountOfPromises = document.querySelector('[name="amount"]').value
+
+  for (let i = 1; i <= amountOfPromises; i += 1) {
+    const currentDelay = (Number(firstDelay) + Number(stepTime) * (i - 1))
+    createPromise(i, currentDelay)
+  }
+})
